7a972e7771080808201241b006b12f99a0978c3f
kalle <kalle.r.persson@gmail.com>
2013-01-20 17:38:20 +0100

    fixat lite i på indexsidan

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,19 +1,33 @@
 @layout('master')
 @section('content')
-<div class="span10">
-	<h1>Öppettider</h1>
 	<div class="span5 leftcol">
+	<br>
+		<h1>Öppettider</h1>
+		<br>
 		<p>
-			Mån - Fre: 07:00 - 19:00<br>
-			Lör        10:00 - 18:00<br>
-			Sön		   12:00 - 16:00
+			<strong>
+				Mån - Fre: 07:00 - 19:00<br><br>
+				Lör        10:00 - 18:00<br><br>
+				Sön		   12:00 - 16:00
+			</strong>
 		</p>
 	</div>
+	<br>
+	
 	<div class="span4 rightcol">
+		<h1>Veckans meny</h1>
+		<br>
 		<p>
-			skdfj
+			<strong>
+				Måndag: Pasta Carbonara<br><br>
+				Tisdag: Spagetti Bolognese<br><br>
+				Onsdag:	Plankstek<br><br>
+				Torsdag: Pizza<br><br>
+				Fredag: Pyttipanna<br><br>
+				Lördag: Stekt kyckling<br><br>
+				Söndag: Piroger
+			</strong>
 		</p>
 	</div>
-</div>
 @endsection 
 
